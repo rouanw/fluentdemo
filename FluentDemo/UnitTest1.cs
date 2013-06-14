3c3f23f9f5a5c483073f738dc73116a6c3dcbd7b
Rouan <rouanw@gmail.com>
2013-06-14 16:03:16 +0300

    initial

---
+++ FluentDemo/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentDemo
+{
+    // Docs: http://fluentassertions.codeplex.com/documentation
+
+    [TestClass]
+    public class FluentDemoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
